7c4bfa659574fe7ebb5483640cc27432d24e5605
jardinargent <stellarebullience@gmail.com>
2016-12-17 21:54:15 -0800

    update to filteredApts

--- config/filteredApts.php
+++ config/filteredApts.php
@@ -4,11 +4,15 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include_once 'database.php';
 include_once 'Apartment.php';
+include_once 'OtherQueries.php';
+
 
 $database = new Database();
 $db = $database->getConnection();
 
 $apt = new Apartment($db);
+$oq = new OtherQueries($db);
+
 //query all
 $request = json_decode(file_get_contents("php://input"),true); //returns array
 
@@ -19,6 +23,33 @@ $data = array();
 
 if($num>0){
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+    $stmt2 = $oq->countTransNear($row['longitude'],$row['latitude']);
+    $countTrans = $stmt2->rowCount();
+
+    if($countTrans > 0){
+      $row['hasNearbyTrans'] = "YES";
+    }else{
+      $row['hasNearbyTrans'] = "NO";
+    }
+
+    $stmt3 = $oq->countSchoolsNear($row['longitude'],$row['latitude']);
+    $countSchools = $stmt3->rowCount();
+
+    if($countSchools > 0){
+      $row['hasNearbySchools'] = "YES";
+    }else{
+      $row['hasNearbySchools'] = "NO";
+    }
+
+    $stmt4 = $oq->countStoresNear($row['longitude'],$row['latitude']);
+    $countStores = $stmt4->rowCount();
+
+    if($countStores > 0){
+      $row['hasNearbyStores'] = "YES";
+    }else{
+      $row['hasNearbyStores'] = "NO";
+    }
     $data[] = $row;
   }
 }
